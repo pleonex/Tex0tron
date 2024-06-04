fb96aed62e2d40427cc19f66d3c8d7b334ff9cf1
Benito Palacios Sanchez <pleonex@protonmail.com>
2024-06-05 00:00:47 +0200

    🔧[web] Add base-href to build

--- build/orchestrator/Angular/BuildTask.cs
+++ build/orchestrator/Angular/BuildTask.cs
@@ -1,5 +1,6 @@
 namespace BuildSystem.Angular;
 
+using System.Text;
 using BuildSystem.Node;
 using Cake.Common;
 using Cake.Core;
@@ -15,13 +16,21 @@ public class BuildTask : FrostingTask<Tex0tronBuildContext>
     {
         string projectName = Path.GetFileName(context.NodeContext.NodeProjectPath)!;
         string outputPath = Path.Combine(context.ArtifactsPath, projectName);
+        context.Log.Information(
+            $"Building Angular project '{projectName}' " +
+            $"with base href {context.GitHubContext.RepositoryName}");
 
-        context.Log.Information($"Building Angular project '{projectName}'");
         string commandExtension = context.IsRunningOnWindows() ? ".cmd" : "";
+        string arguments = new StringBuilder()
+            .Append("ng build")
+            .Append(" --configuration ").Append(context.AngularContext.Configuration)
+            .Append(" --output-path ").Append(outputPath)
+            .AppendFormat(" --base-href /{0}/", context.GitHubContext.RepositoryName)
+            .ToString();
         int retcode = context.StartProcess(
             "npx" + commandExtension,
             new Cake.Core.IO.ProcessSettings {
-                Arguments = $"ng build --configuration {context.AngularContext.Configuration} --output-path {outputPath}",
+                Arguments = arguments,
                 WorkingDirectory = context.NodeContext.NodeProjectPath,
             });
         if (retcode != 0) {
